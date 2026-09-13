9111e41fad96a17ef252ccb6f9e09032b5085d98
stekokisel-crypto <steko.kisel@gmail.com>
2026-09-13 13:40:25 +0300

    Update Maim.py

--- Maim.py
+++ Maim.py
@@ -23,7 +23,7 @@ async def chat_with_groq(message: types.Message):
             messages=[
                 {"role": "user", "content": message.text}
             ],
-            model="qwen/qwen3.6-27b",
+            model="openai/gpt-oss-20b",
         )
         answer = chat_completion.choices[0].message.content
         await message.answer(answer)
