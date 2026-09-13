211357268ba88e8dbe2bf4be5ff9076284688516
stekokisel-crypto <steko.kisel@gmail.com>
2026-09-13 13:20:37 +0300

    Update Maim.py

--- Maim.py
+++ Maim.py
@@ -23,7 +23,7 @@ async def chat_with_groq(message: types.Message):
             messages=[
                 {"role": "user", "content": message.text}
             ],
-            model="llama3-8b-8192-versatile",
+            model="llama3-8b-8192",
         )
         answer = chat_completion.choices[0].message.content
         await message.answer(answer)
